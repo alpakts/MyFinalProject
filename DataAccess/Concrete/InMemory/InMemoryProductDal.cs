ae76857dddc34ef13d1d1aa5c88c0db1f1b48d9e
alper-aktas <alper-aktas@ALPER>
2022-05-29 20:05:09 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,59 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products; // global değişken
+        public InMemoryProductDal()
+        {
+            // Oracle,Sql server,Postgres,MongoDb den veri simülasyonu
+            _products = new List<Product> {
+            new Product{ProductId=1,CategoryId=1,ProductName="Bardak",UnitPrice=15,UnitInStcok=15},
+            new Product{ProductId=2,CategoryId=1,ProductName="Kamera",UnitPrice=1000,UnitInStcok=2},
+            new Product{ProductId=3,CategoryId=2,ProductName="Bilgisayar",UnitPrice=1500,UnitInStcok=10},
+            new Product{ProductId=4,CategoryId=2,ProductName="Sandalye",UnitPrice=200,UnitInStcok=152},
+            new Product{ProductId=5,CategoryId=2,ProductName="Masa",UnitPrice=400,UnitInStcok=30}
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            // LINQ Language Integrated Query
+            Product productToDelete = _products.SingleOrDefault(p=>p.ProductId==product.ProductId);// linq kullanımı 
+                                               //foreach gibi çalışan method
+            _products.Remove(productToDelete);
+            
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {   // gönderdiğim ürün id'sine sahip olan listedeki ürünü bul 
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitInStcok = product.UnitInStcok;
+
+        }
+    }
+}
---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Abstract
+{
+    // Ientitity interfacesini implement eden her class veritabanı classıdır
+    public interface IEntity
+    {
+    }
+}
